a4dc7725db1e28ced045285e7f3792dad66e59e3
rtr1792 <rtr1792@gmail.com>
2017-02-10 19:17:22 -0600

    Lot's of Drive and Other thing
    +CANed up the Drive
    -Removed Testing Code
    +Created the XBox Controls for FC-RC
    +Added Skeleton for US
    +Reworked ports for different Elements
    +Added Latch for FC-RC
    +Added Latch for Gear Flaps
    +Added some testing code for the Flaps

--- Getting Started/src/Robot.cpp
+++ Getting Started/src/Robot.cpp
@@ -11,7 +11,7 @@
 #include <iostream>
 #include <iomanip>
 #include <unistd.h>
-
+#include "CANTalon.h"
 
 #define POT_MAX_VALUE 40
 #define POT_MIN_VALUE 10
@@ -69,6 +69,8 @@ int GetChannelFromPin( PinType type, int io_pin_number ) {
     }
     return roborio_channel;
 }
+
+
 //When deploying Code: Turn off Wifi
 
 //If there is an Error With Microsoft Visual Studios: Go to Project->Properties->
@@ -76,18 +78,17 @@ int GetChannelFromPin( PinType type, int io_pin_number ) {
 //Hit Apply-> Uncheck CDT Cross GCC Built-in Compiler Settings-> Hit Apply
 //Reckeck CDT Cross GCC Built-in Complier Settings-> Hit Apply-> close and build
 class Robot: public frc::IterativeRobot {
+
 public:
 
-	Robot() {
+	Robot() : lf(2), lr(1), rf(4), rr(5){
 		//Generic initialization code
-		myRobot.SetExpiration(0.1);
+		m_robotDrive.SetExpiration(0.1);
 		//Inverts the left side
-		myRobot.SetInvertedMotor(RobotDrive::kFrontLeftMotor, true);
-		myRobot.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);
+		m_robotDrive.SetInvertedMotor(RobotDrive::kFrontLeftMotor, true);
+		m_robotDrive.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);
 		timer.Start();  //Initializes the timer
 		autocounter = 0;
-
-		pwm_out_0 = new Victor(        GetChannelFromPin( PinType::PWMs,       0 ));
 
 
 		//This try catch block checks to see if an error instantiating the navx is thrown
@@ -117,89 +118,31 @@ public:
 	}
 
 private:
-	//
-
-	/*
-	//Talons for testing the PWM output
-	frc::TalonSRX testTalon0 { 0 };
-	frc::TalonSRX testTalon1 { 1 };
-	frc::TalonSRX testTalon2 { 2 };
-	frc::TalonSRX testTalon3 { 3 };
-	frc::TalonSRX testTalon4 { 4 };
-	frc::TalonSRX testTalon5 { 5 };
-	frc::TalonSRX testTalon6 { 6 };
-	frc::TalonSRX testTalon7 { 7 };
-	frc::TalonSRX testTalon8 { 8 };
-	frc::TalonSRX testTalon9 { 9 };
-	 */
-
-	/*
-	//Buttons for testing DIO input
-	frc::DigitalInput testButton0 { 0 };
-	frc::DigitalInput testButton1 { 1 };
-	frc::DigitalInput testButton2 { 2 };
-	frc::DigitalInput testButton3 { 3 };
-	frc::DigitalInput testButton4 { 4 };
-	frc::DigitalInput testButton5 { 5 };
-	frc::DigitalInput testButton6 { 6 };
-	frc::DigitalInput testButton7 { 7 };
-	frc::DigitalInput testButton8 { 8 };
-	frc::DigitalInput testButton9 { 9 };
-	*/
-
-	/*
-	//LEDs for testing DIO Output
-	frc::DigitalOutput testLED0 { 0 };
-	frc::DigitalOutput testLED1 { 1 };
-	frc::DigitalOutput testLED2 { 2 };
-	frc::DigitalOutput testLED3 { 3 };
-	frc::DigitalOutput testLED4 { 4 };
-	frc::DigitalOutput testLED5 { 5 };
-	frc::DigitalOutput testLED6 { 6 };
-	frc::DigitalOutput testLED7 { 7 };
-	frc::DigitalOutput testLED8 { 8 };
-	frc::DigitalOutput testLED9 { 9 };
-	*/
-
-	//Potentiometers for testing PWM input
-//	frc::PWM testPot0 { 0 };
-//	frc::PWM testPot1 { 1 };
-//	frc::PWM testPot2 { 2 };
-//	frc::PWM testPot3 { 3 };
-//	frc::PWM testPot4 { 4 };
-//	frc::PWM testPot5 { 5 };
-//	frc::PWM testPot6 { 6 };
-//	frc::Potentiometer testPot7 { 7 };
-//	frc::Potentiometer testPot8 { 8 };
-//	frc::Potentiometer testPot9 { 9 };
-
-
-
-
-
 	frc::Joystick stick { 0 };         // Only joystick
-
+	frc::XboxController xbox { 1 };
 
 	AHRS *ahrs;
 	// 0  2
 	// 1  3
 
-	/*
-	//Motors for Driving
-	TalonSRX frontLeftMotor { 0 };
-	TalonSRX frontRightMotor { 1 };
-	TalonSRX rearLeftMotor { 2 };
-	TalonSRX rearRightMotor { 3 };
-	*/
+	//Ultrasonic Variables
+	frc::DigitalOutput gearUSTrig { 0 };
+	frc::DigitalInput gearUSEcho { 1 };
+	float gearUSTimeStart = 0;
+	bool gearTimeReset = 0;
 
-	frc::RobotDrive myRobot {1,2,3,4};
-	frc::TalonSRX groundIntakeMotor { 5}; //Motor for the ground intake
-	frc::TalonSRX outakeMotor { 6 }; //Motor for the cloth lifting thing
-	frc::TalonSRX climberMotor { 7 }; //Motor for climbing
-//	frc::Ultrasonic ultrasonicTest { 9 };  //Test code for the ultrasonic
+	CANTalon lf; /*left front */
+	CANTalon lr;/*left rear */
+	CANTalon rf; /*right front */
+	CANTalon rr; /*right rear */
 
-	Victor *pwm_out_0 ;
-
+	frc::RobotDrive m_robotDrive {lf, lr, rf, rr};
+	frc::TalonSRX groundIntakeMotor { 1 }; //Motor for the ground intake
+	frc::TalonSRX outakeMotor { 2 }; //Motor for the cloth lifting thing
+	frc::TalonSRX climberMotor { 3 }; //Motor for climbing
+	frc::Servo gearServoLeft { 4 };
+	frc::Servo gearServoRight { 5 };
+\
 
 
 	//Values for determining a deadband for control
@@ -213,6 +156,15 @@ private:
 	float rearLeft = 0;
 	float rearRight = 0;
 
+	//Drive Latch Variables
+	bool driveToggle = false;
+	float driveGyro = 0;
+	bool driveLatch = false;
+
+	//GO Latch Variables
+	bool gearToggle = false;
+    bool gearLatch = false;
+
 	//Temporary Value(s)
 	float tempA = 0;
 
@@ -225,6 +177,7 @@ private:
 	void AutonomousInit() override {
 		timer.Reset();
 		timer.Start();
+
 //		std::shared_ptr<NetworkTable> table = NetworkTable::GetTable("datatable");
 //		lw = LiveWindow::GetInstance();
 //		try {
@@ -247,11 +200,11 @@ private:
 				//If the autocounter is less than 100, move forward and add 1 to the autocounter
 				while(autocounter<AUTO_ONE)
 				{
-					myRobot.MecanumDrive_Cartesian(0, 1,0, 0);
+					m_robotDrive.MecanumDrive_Cartesian(0, 1,0, 0);
 					autocounter++;
 					if(autocounter == AUTO_ONE)
 					{
-						myRobot.MecanumDrive_Cartesian(0,0,0,0);
+						m_robotDrive.MecanumDrive_Cartesian(0,0,0,0);
 					}
 				}
 				//If the autocounter is equal to 100, stop the driving
@@ -261,33 +214,33 @@ private:
 				{
 					if(ahrs->GetAngle()<ANGLE+WIGGLEROOM)
 					{
-						myRobot.MecanumDrive_Cartesian(0, 0,1, 0);
+						m_robotDrive.MecanumDrive_Cartesian(0, 0,1, 0);
 					}
 					else
 					{
-						myRobot.MecanumDrive_Cartesian(0, 0,-1, 0);
+						m_robotDrive.MecanumDrive_Cartesian(0, 0,-1, 0);
 					}
 				}
 				//go field centric forward a set time (UNTIL_PEG_TIME)
 				autocounter = 0;
 				while(autocounter<AUTO_TWO)
 				{
-					myRobot.MecanumDrive_Cartesian(0, 1,0,ahrs->GetAngle());
+					m_robotDrive.MecanumDrive_Cartesian(0, 1,0,ahrs->GetAngle());
 			        autocounter++;
 			        if(autocounter == AUTO_TWO)
 			        {
-			        	myRobot.MecanumDrive_Cartesian(0,0,0,0);
+			        	m_robotDrive.MecanumDrive_Cartesian(0,0,0,0);
 			        }
 				}
 
 			//robot centric forward for te set time (UNTIL_GEAR_AT_PEG_TIME)
 				while(autocounter<AUTO_THREE)
 				{
-					myRobot.MecanumDrive_Cartesian(0, 1,0,0);
+					m_robotDrive.MecanumDrive_Cartesian(0, 1,0,0);
 					autocounter++;
 					if(autocounter == AUTO_THREE)
 					{
-						myRobot.MecanumDrive_Cartesian(0,0,0,0);
+						m_robotDrive.MecanumDrive_Cartesian(0,0,0,0);
 					}
 				}
 				//wait until us sensor says gear is out
@@ -295,21 +248,21 @@ private:
 				// go robot centric back a set time (UNTIL_OUT_OF_THE_WAY_TIME)
 				while(autocounter<AUTO_FOUR)
 				{
-					myRobot.MecanumDrive_Cartesian(0, -1,0,0);
+					m_robotDrive.MecanumDrive_Cartesian(0, -1,0,0);
 					autocounter++;
 					if(autocounter == AUTO_FOUR)
 					{
-						myRobot.MecanumDrive_Cartesian(0,0,0,0);
+						m_robotDrive.MecanumDrive_Cartesian(0,0,0,0);
 					}
 				}
 			//go field centric forward a set time (BASE_LINE_TIME)
 				 while(autocounter<AUTO_FIVE)
 				 {
-				 	myRobot.MecanumDrive_Cartesian(0, -1,0,ahrs->GetAngle());
+				 	m_robotDrive.MecanumDrive_Cartesian(0, -1,0,ahrs->GetAngle());
 				 	autocounter++;
 				 	if(autocounter == AUTO_FIVE)
 				 	{
-				 		myRobot.MecanumDrive_Cartesian(0,0,0,0);
+				 		m_robotDrive.MecanumDrive_Cartesian(0,0,0,0);
 				 	}
 				 }
 				};
@@ -321,88 +274,7 @@ private:
 	void TeleopPeriodic() override {
 
 
-		//These functions are just for testing purposes
 
-		/*
-		//Testing PWM Output
-		if(stick.GetRawButton(5))//If button 5 is pushed, motors runs forward
-		{
-			testTalon0.SetSpeed(0.25); //Motors go forward at 25% speed
-			testTalon1.SetSpeed(0.25);
-			testTalon2.SetSpeed(0.25);
-			testTalon3.SetSpeed(0.25);
-			testTalon4.SetSpeed(0.25);
-			testTalon5.SetSpeed(0.25);
-			testTalon6.SetSpeed(0.25);
-			testTalon7.SetSpeed(0.25);
-			testTalon8.SetSpeed(0.25);
-			testTalon9.SetSpeed(0.25);
-		}
-		else //If button is not pressed
-		{
-			testTalon0.SetSpeed(0); //Stop motors
-			testTalon1.SetSpeed(0);
-			testTalon2.SetSpeed(0);
-			testTalon3.SetSpeed(0);
-			testTalon4.SetSpeed(0);
-			testTalon5.SetSpeed(0);
-			testTalon6.SetSpeed(0);
-			testTalon7.SetSpeed(0);
-			testTalon8.SetSpeed(0);
-			testTalon9.SetSpeed(0);
-		}
-		*/
-
-		/*
-		//Testing the DIOs input by presenting to the SmartDashboard
-		frc::SmartDashboard::PutBoolean("Button 0",testButton0.Get());
-		frc::SmartDashboard::PutBoolean("Button 1",testButton1.Get());
-		frc::SmartDashboard::PutBoolean("Button 2",testButton2.Get());
-		frc::SmartDashboard::PutBoolean("Button 3",testButton3.Get());
-		frc::SmartDashboard::PutBoolean("Button 4",testButton4.Get());
-		frc::SmartDashboard::PutBoolean("Button 5",testButton5.Get());
-		frc::SmartDashboard::PutBoolean("Button 6",testButton6.Get());
-		frc::SmartDashboard::PutBoolean("Button 7",testButton7.Get());
-		frc::SmartDashboard::PutBoolean("Button 8",testButton8.Get());
-		frc::SmartDashboard::PutBoolean("Button 9",testButton9.Get());
-		*/
-
-		/*
-		//Testing DIOs ouput by turning on LEDs
-		//If button is pushed, do below
-		if(stick.GetRawButton(5))
-		{
-			//Turn on LEDs
-			testLED0.Set(1);
-			testLED1.Set(1);
-			testLED2.Set(1);
-			testLED3.Set(1);
-			testLED4.Set(1);
-			testLED5.Set(1);
-			testLED6.Set(1);
-			testLED7.Set(1);
-			testLED8.Set(1);
-			testLED9.Set(1);
-		}
-		else//If button is not pushed, do below
-		{
-			//Turn off LEDs
-			testLED0.Set(0);
-			testLED1.Set(0);
-			testLED2.Set(0);
-			testLED3.Set(0);
-			testLED4.Set(0);
-			testLED5.Set(0);
-			testLED6.Set(0);
-			testLED7.Set(0);
-			testLED8.Set(0);
-			testLED9.Set(0);
-		}
-		*/
-
-		//Sets the variable gyroAngle to the value that the Gyro has
-		//gyroAngle = gyro.GetRaw();
-		//gyroAngle = 5;
 		//Drives the robot using the joystick, the gyro, and Mecanum
 		//if the value of the stick is less than 10%, set to 0
 		if (abs(stick.GetX()) < .1)
@@ -435,38 +307,30 @@ private:
 			joystickDeadBandZ = stick.GetZ();
 		}
 
-		//MATH!!! Copied code from Chief Delphi. Member Ether
-		frontLeft = joystickDeadBandY + joystickDeadBandZ + joystickDeadBandX;
-		frontRight = joystickDeadBandY - joystickDeadBandZ - joystickDeadBandX;
-		rearLeft = joystickDeadBandY + joystickDeadBandZ - joystickDeadBandX;
-		rearRight = joystickDeadBandY - joystickDeadBandZ + joystickDeadBandX;
 
 
-		//Somehow determines a max
-		driveMax = abs(frontLeft);
-		tempA = abs(frontRight);
-		if(tempA > driveMax)
+		//Latch For Gyro
+		if(xbox.GetRawButton(5)&&xbox.GetRawButton(6)&&!driveLatch)
 		{
-			driveMax = tempA;
+				driveToggle=!driveToggle;
+				driveLatch = true;
 		}
-		tempA = abs(rearLeft);
-		if(tempA > driveMax)
+		else if(!xbox.GetRawButton(5)&&!xbox.GetRawButton(6)&&driveLatch)
 		{
-			driveMax = tempA;
+			driveLatch = false;
 		}
-		tempA = abs(rearRight);
-		if(tempA > driveMax)
+		//Robot Centric
+		if(driveToggle)
 		{
-			driveMax = tempA;
+			driveGyro = 0;
+		}
+		//Field Centric
+		else if(!driveToggle)
+		{
+			driveGyro = ahrs->GetAngle();
 		}
 
-		if(driveMax >1)
-		{
-			frontLeft /= driveMax;
-			frontRight /= driveMax;
-			rearLeft /= driveMax;
-			rearRight /= driveMax;
-		}
+
 
 		//Set values to each motor
 		//frontLeftMotor.SetSpeed(frontLeft);
@@ -480,11 +344,11 @@ private:
 			ahrs->ZeroYaw();
 		}
 		try {
-			/* Use the joystick X axis for lateral movement,            */
-			/* Y axis for forward movement, and Z axis for rotation.    */
-			/* Use navX MXP yaw angle to define Field-centric transform */
-			myRobot.MecanumDrive_Cartesian(stick.GetX(), stick.GetY(),
-					stick.GetZ(), ahrs->GetAngle());
+			/* Use the joystick X axis for lateral movement,
+			 Y axis for forward movement, and Z axis for rotation.
+			 Use navX MXP yaw angle to define Field-centric transform */
+			m_robotDrive.MecanumDrive_Cartesian(stick.GetX(), stick.GetY(),
+					stick.GetZ(),driveGyro);
 		} catch (std::exception& ex) {
 			std::string err_string = "Error communicating with Drive System:  ";
 			err_string += ex.what();
@@ -496,6 +360,27 @@ private:
 
 		//AHRS has a check PWM not push PWM so they can only be used for sensors
 
+
+		//Latch for GO
+		if(stick.GetRawButton(2)&&!gearLatch)
+		{
+			gearToggle=!gearToggle;
+			gearLatch=true;
+		}
+		else if(!stick.GetRawButton(2)&&gearLatch)
+		{
+			gearLatch = false;
+		}
+		//Open Flaps
+		if(gearToggle)
+		{
+			//open the flaps
+		}
+		//Close Flaps
+		else if(!gearToggle)
+		{
+			//close the flaps
+		}
 
 
 
@@ -514,6 +399,19 @@ private:
 		{
 			groundIntakeMotor.SetSpeed(0);
 		}
+
+
+		if(!gearTimeReset)
+		{
+			//timer.Get()=gearUSTimeStart;
+			gearTimeReset = true;
+		}
+		if((timer.Get()-gearUSTimeStart)==0)
+		{
+			gearUSTrig.Set(false);
+		}
+        else if((timer.Get()-gearUSTimeStart)==2)
+
 
 
 		//If button 5 is pressed, the outake motor will spin forwards at half power until pot is greater than 40
@@ -543,6 +441,18 @@ private:
 		{
 			climberMotor.SetSpeed(0);
 		}
+
+		if(stick.GetRawButton(7))
+		{
+			gearServoLeft.SetAngle(20);
+		}
+
+		else if(stick.GetRawButton(8))
+		{
+			gearServoLeft.SetAngle(-20);
+		}
+
+		frc::SmartDashboard::PutNumber("Servo Angle",gearServoLeft.GetAngle());
 
 
 		//frc::SmartDashboard::PutNumber("height of gear",gearUlt.GetRangeInches());
